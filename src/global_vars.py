5d1c8d82af95fa0aa8ada415fdc2905128b49fce
unknown <ned1_32@sina.com>
2017-12-10 15:46:54 +0800

    add ai controller and map driving function

--- src/global_vars.py
+++ src/global_vars.py
@@ -2,12 +2,17 @@ from typing import Dict
 import terrain
 import cls
 import person
+import ai_controller
 class Main:
     def __init__(self):
         self.terrainBank={}                 #type:Dict[str,terrain.Terrain]
         self.clsBank={}                     #type:Dict[str,cls.Cls]
         self.personBank={}                  #type:Dict[str,person.Person]
+        self.cls_clsgroup={}                #type:Dict[str,str]
+        self.AIcontroller=None             #type:ai_controller.AI_Controller
     def __init__(self,data):
         self.terrainBank=terrain.Main(data).terrain_bank
         self.clsBank=cls.Main(data).cls_bank
         self.personBank=person.Main(data).person_bank
+        self.cls_clsgroup=data.cls_clsgroup
+        self.AIcontroller=ai_controller.AI_Controller()
