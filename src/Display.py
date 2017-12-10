e08311b9a7e9644b4fbc54ad8760bd351053b8eb
antastsy <664692054@qq.com>
2017-12-10 15:02:50 +0800

    display

--- src/Display.py
+++ src/Display.py
@@ -1,11 +1,13 @@
 import cocos
 from cocos.sprite import Sprite
 from cocos.director import director
+from cocos.actions import MoveTo
 from data_loader import Main as Data
 from global_vars import Main as Global
 from terrain_container import Main as Terrain_Container
 from person_container import Main as Person_Container
 import map_controller
+from colors import *
 class Arena(cocos.layer.ColorLayer):
     is_event_handler = True
 
@@ -25,47 +27,51 @@ class Arena(cocos.layer.ColorLayer):
                 self.add(Tile(pos=(x * self.size+self.size//2, y * self.size+self.size//2), color=(255, 255, 255), size=self.size))
         self.repaint(map1)
 
+    def transform_xy(self, x, y):
+        pass
+
+    def move(self, id, x, y):
+        s = self.person[id]
+        mov = MoveTo((s.position[0], s.position[1]), 2) + MoveTo((x * self.size+self.size//2, y * self.size+self.size//2), 2)
+
+        s.do(mov)
+
 
     def repaint(self, map_controller):
         position = map_controller.person_container.position
         army = map_controller.person_container.army
+        self.person = {}
         for id in position:
             (y, x) = position[id]
             if army[id] == "Lyn's Army" :
-                color = (135, 206, 235)
+                color = ORANGE
             else:
-                color = (255, 165, 0)
-            self.add(Tile(pos=(x * self.size+self.size//2, y * self.size+self.size//2), color=color, size=self.size))
+                color = SKY_BLUE
+            self.person[id] = Ally(pos=(x * self.size+self.size//2, y * self.size+self.size//2), color=color, size=self.size)
+            self.add(self.person[id])
+        self.move('1', 3, 3)
 
 
-        pass
+
 
 class Tile(Sprite):
-    def __init__(self, size=50,pos=None, color=(135, 206, 235)):
-        super(Tile, self).__init__(image='circle.png')
+    def __init__(self, size=50,pos=None):
+        super(Tile, self).__init__(image='img/circle.png')
         self.scale = size/self.height
-        self.color = color
+        self.color = (255, 255, 255)
         self.position = pos
 
 
 
 class Ally(Sprite):
-    def __init__(self, pos=None, color=None):
-        if color is None:
-            color = random.choice(define.ALL_COLOR)
+    def __init__(self, size=50,pos=None, color=(135, 206, 235)):
+        super(Tile, self).__init__(image='img/circle.png')
+        self.scale = size/self.height
+        self.color = color
+        self.position = pos
 
-        super(Dot, self).__init__('circle.png', color=color)
-        self.killed = False
-        if pos is None:
-            self.position = (random.randint(40, define.WIDTH - 40),
-                             random.randint(40, define.HEIGHT - 40))
-            self.is_big = False
-            self.scale = 0.8
-        else:
-            self.position = (pos[0] + random.random() * 32 - 16,
-                             pos[1] + random.random() * 32 - 16)
-            self.is_big = True
-        self.schedule_interval(self.update, random.random() * 0.2 + 0.1)
+    def on_mouse_press(self, x, y, buttons, modfiers):
+        print(x, y)
 
 if __name__ == '__main__':
     director.init(caption='3X')
