aa326f3bff1a62ea63dba880117ea7481ae01c5b
antastsy <664692054@qq.com>
2017-12-10 23:07:44 +0800

    on_mouse_motion

--- src/Display.py
+++ src/Display.py
@@ -7,28 +7,32 @@ from global_vars import Main as Global
 from terrain_container import Main as Terrain_Container
 from person_container import Main as Person_Container
 import map_controller
-from colors import *
+from utility import *
 class Arena(cocos.layer.ColorLayer):
     is_event_handler = True
 
     def __init__(self):
         self.size = 80
+        self.select = (0, 0)
+        self.origin_color = WHITE
         data = Data()
         global_vars = Global(data)
         terrain_container_test = Terrain_Container(data.terrain_map, global_vars.terrainBank)
         person_container_test = Person_Container(data.map_armylist, global_vars.personBank)
         map1 = map_controller.Main(terrain_container_test, person_container_test, data)
-        w = terrain_container_test.N
-        h = terrain_container_test.M
+        self.w = terrain_container_test.N
+        self.h = terrain_container_test.M
 
-        super(Arena, self).__init__(r=0,g=0,b=0,a=255,width=w*self.size,height=h*self.size)
-        for x in range(w):
-            for y in range(h):
-                self.add(Tile(pos=(x * self.size+self.size//2, y * self.size+self.size//2), color=(255, 255, 255), size=self.size))
+        super(Arena, self).__init__(r=0,g=0,b=0,a=255,width=self.w*self.size,height=self.h*self.size)
+        self.tiles = []
+        for x in range(self.w):
+            tl_x = []
+            for y in range(self.h):
+                tile = Tile(pos=coordinate(x, y, self.size), size=self.size)
+                self.add(tile)
+                tl_x.append(tile)
+            self.tiles.append(tl_x)
         self.repaint(map1)
-
-    def transform_xy(self, x, y):
-        pass
 
     def move(self, id, x, y):
         s = self.person[id]
@@ -52,6 +56,15 @@ class Arena(cocos.layer.ColorLayer):
         self.move('1', 3, 3)
 
 
+    def on_mouse_motion(self, x, y, buttons, modifiers):
+        i, j = coordinate_t(x, y, self.size)
+        print(i, j)
+        if i in range(0, self.w) and j in range(0, self.h):
+            i0, j0 = self.select
+            self.tiles[i0][j0].color = self.origin_color
+            self.origin_color = self.tiles[i][j].color
+            self.tiles[i][j].color = LIGHT_PINK
+            self.select = i, j
 
 
 class Tile(Sprite):
@@ -65,7 +78,7 @@ class Tile(Sprite):
 
 class Ally(Sprite):
     def __init__(self, size=50,pos=None, color=(135, 206, 235)):
-        super(Tile, self).__init__(image='img/circle.png')
+        super(Ally, self).__init__(image='img/circle.png')
         self.scale = size/self.height
         self.color = color
         self.position = pos
---
+++ src/utility.py
@@ -0,0 +1,29 @@
+
+def coordinate(i, j, size):
+    x = i * size + size // 2
+    y = j * size + size // 2
+    return x, y
+
+def coordinate_t(x, y, size):
+    i = x // size
+    j = y // size
+    return i, j
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_PINK = (255, 182, 193)
+STEEL_BLUE = (70, 130, 180)
+CRIMSON = (220, 20, 60)
+VIOLET = (238, 130, 238)
+SLATEBLUE = (106, 90, 205)
+CYAN = (0, 255, 255)
+AUQAMARIN = (127, 255, 170)
+LIME = (0, 255, 0)
+YELLOW = (255, 255, 0)
+OLIVE = (128, 128, 0)
+CORNISLK = (255, 248, 220)
+ORANGE = (255, 165, 0)
+CORAL = (255, 127, 80)
+SKY_BLUE = (135, 206, 235)
+GOLD = (255, 215, 0)
+MAROON = (128, 0, 0)
