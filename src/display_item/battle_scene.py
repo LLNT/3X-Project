bcc7af3a31b3e98dd9b3d96c52276b07cd92326c
chenxinyun <664692054@qq.com>
2018-02-04 21:10:20 +0800

    add text

--- src/display_item/battle_scene.py
+++ src/display_item/battle_scene.py
@@ -6,13 +6,15 @@
 
 from cocos.director import director
 from cocos.layer import ColorLayer
-from cocos.scenes import ShuffleTransition
+from cocos.scenes import FadeTransition
 from cocos.scene import Scene
 from display_item.text import Text
 from battle import Battle
 from cocos.sprite import Sprite
 from cocos.layer import Layer
-from cocos.actions import RotateTo, RotateBy, Delay, CallFunc
+from cocos.actions import RotateTo, RotateBy, Delay, CallFunc, MoveBy, MoveTo, FadeIn, FadeOut
+from queue import Queue
+
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
@@ -20,27 +22,37 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 class Battlescene(ColorLayer):
-    is_event_handler = True
-    def __init__(self, battlelist, w=800, h=600):
+    is_event_handler = False
+    def __init__(self, battlelist, w=800, h=600, maxsize=2):
         super(Battlescene, self).__init__(255,255,255,255,width=w, height=h)
-        at, df, wp, map, pos = battlelist
-        battle = Battle(at, df, wp, df.item[0], map, pos)
+
+        self.at, self.df, wp, map, pos = battlelist
+        battle = Battle(self.at, self.df, wp, self.df.item[0], map, pos)
         pos1 = w // 4, h // 3
-        hp1, mhp1 = at.ability['HP'], at.ability['MHP']
-        p1 = Ring(pos1, 0.4, prop=hp1 / mhp1, back_color = WHITE, hp=hp1, mhp=mhp1)
+        self.hp1, self.mhp1 = self.at.ability['HP'], self.at.ability['MHP']
+
+        self.attacker = Ring(pos1, 0.4, prop=self.hp1 / self.mhp1, back_color = WHITE, hp=self.hp1, mhp=self.mhp1)
         pos2 = w  *6 // 11, h // 3
-        hp2, mhp2 = df.ability['HP'], df.ability['MHP']
-        p2 = Ring(pos2, 0.4, prop=hp2 / mhp2,back_color = WHITE, hp=hp2, mhp=mhp2)
-        self.add(p1)
-        self.add(p2)
+        self.hp2, self.mhp2 = self.df.ability['HP'], self.df.ability['MHP']
+        self.defender = Ring(pos2, 0.4, prop=self.hp2 / self.mhp2,back_color = WHITE, hp=self.hp2, mhp=self.mhp2)
+
+        self.add(self.attacker)
+        self.add(self.defender)
 
         res = battle.battle()
         del battle
         self.w = w
         self.h = h
-        print(res)
         action1 = action2 = Delay(0.5)
-        for event in res:
+
+        self.events = res
+        self.i = -1
+        self.obj1 = self.attacker.right_ring
+        self.obj2 = self.defender.right_ring
+
+
+        '''for event in res:
+            print(event)
             if event[0] < 0:
                 continue
             hit,dmg,amg = event[1].split(',')
@@ -48,26 +60,24 @@ class Battlescene(ColorLayer):
             amg = int(amg)
             if hit is 'H':
                 if event[0] is 1:
-                    hp2 -= dmg
-                    hp1 += amg
+                    self.hp2 -= dmg
+                    self.hp1 += amg
                 elif event[0] is 2:
-                    hp1 -= dmg
-                    hp2 += amg
-
+                    self.hp1 -= dmg
+                    self.hp2 += amg
                 # p1.set_angle(hp1 / mhp1)
                 # p2.set_angle(hp2 / mhp2)
-
-                _, a1 = p1.set_angle_action(hp1 / mhp1)
-                _, a2 = p2.set_angle_action(hp2 / mhp2)
+                _, a1 = self.attacker.set_angle_action(self.hp1 / self.mhp1)
+                _, a2 = self.defender.set_angle_action(self.hp2 / self.mhp2)
                 action1 = action1 + a1
                 action2 = action2 + a2
             else:
                 continue
-        t1 = p1.right_ring
-        t2 = p2.right_ring
-        t1.do(action1)
-        t2.do(action2)
-
+        
+        self.obj1.do(action1)
+        self.obj2.do(action2)'''
+        self.info = Queue(maxsize)
+        self.get_next_action()
         '''info = Info()
         self.add(info)
         content = []
@@ -75,13 +85,86 @@ class Battlescene(ColorLayer):
             content.append(str(event[0])+','+event[1])
         info.display(content)'''
 
+    def get_next_action(self, obj1=False, obj2=False):
+        '''
 
+        :return: the next action to call
+        '''
+
+        if self.obj1.busy or self.obj2.busy:
+            return
+        self.i += 1
+        if self.i >= len(self.events):
+            if self.i == len(self.events):
+                director.window.push_handlers(self)
+            return
+        event = self.events[self.i]
+        print(event)
+        color = (0, 255, 0, 255)
+        if event[0] < 0: # show info on screen
+            content = event[1]
+            self.add_new_infos(content)
+            pass
+        else:
+            hit, dmg, amg = event[1].split(',')
+            dmg = int(dmg)
+            amg = int(amg)
+            if event[0] is 1:
+                self.hp2 -= dmg
+                self.hp1 += amg
+                if hit is 'H':
+                    content = "Hit by " + str(dmg) + ' Damage'
+                elif hit is 'C':
+                    content = 'Critical Attack by ' + str(dmg) + ' Damage'
+                elif hit is 'M':
+                    content = 'Miss'
+                    color = (255, 0, 0, 255)
+                else:
+                    content = 'oooops'
+            elif event[0] is 2:
+                color = (255, 255, 0, 255)
+                self.hp1 -= dmg
+                self.hp2 += amg
+                if hit is 'H':
+                    content = "Reflect by " + str(dmg) + ' Damage'
+                elif hit is 'C':
+                    content = 'Critical Reflect by ' + str(dmg) + ' Damage'
+                elif hit is 'M':
+                    content = 'Reflect Miss'
+                    color = (255, 0, 0, 255)
+                else:
+                    content = 'oooops'
+            self.add_new_infos(content, color)
+
+            t1, a1 = self.attacker.set_angle_action(self.hp1 / self.mhp1)
+            t2, a2 = self.defender.set_angle_action(self.hp2 / self.mhp2)
+            t1.busy = t2.busy = True
+            t1.do(a1 + CallFunc(t1.parent.set_busy) + CallFunc(self.get_next_action))
+            t2.do(a2 + CallFunc(t2.parent.set_busy) + CallFunc(self.get_next_action))
+
+
+
+            # show leaving effect
+            pass
+
+    def add_new_infos(self, content, color = (0, 255, 0, 255)):
+        if self.info.full():
+            obj_leave = self.info.get()
+            obj_leave.do(MoveTo((self.width // 3, self.height), 1))
+        for obj in self.info.queue:
+            obj.do(MoveBy((0, 40), 1))
+        obj = Text(content=content, position=(self.width // 3, self.height // 2), color=color)
+        self.add(obj)
+        self.info.put(obj)
+        obj.do(FadeIn(1.5) + CallFunc(self.get_next_action))
 
     def on_mouse_press(self, x, y, buttons, modifiers):
         self.do(CallFunc(self._return_arena)+ Delay(1) + CallFunc(self._pop))
 
+
     def _return_arena(self):
-        director.push(ShuffleTransition(Scene(ColorLayer(0, 0, 0, 0, self.w, self.h)), duration=1.5))
+        director.window.remove_handlers(self)
+        director.push(FadeTransition(Scene(ColorLayer(0, 0, 0, 0, self.w, self.h)), duration=1.5))
 
     def _pop(self):
         director.pop()
@@ -112,6 +195,7 @@ class Ring(Layer):
         self.add(self.left_ring)
         self.add(self.right_ring)
         self.add(self.mask_ring)
+        self.right_ring.busy = False
         angle = 360 - prop * 360
         print(angle)
         self.angles = angle
@@ -121,6 +205,9 @@ class Ring(Layer):
             self.left_visible()
         self.right_ring.rotation = angle
         self.schedule(self.update)
+
+    def set_busy(self):
+        self.right_ring.busy = False
 
     def set_angle(self, proportion, duration=4):
         angle = 360 - proportion * 360
