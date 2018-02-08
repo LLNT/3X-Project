7c9ece13ffaf45a9bfc190a9724a9fbd15ca1e60
unknown <ned1_32@sina.com>
2018-02-08 17:27:29 +0800

    equip and dequip

--- src/person.py
+++ src/person.py
@@ -15,6 +15,7 @@ class Person:
         self.supdata={}      #type:Dict[str,Tuple[int,int]]
         self.attribute=""    #type:str
         self.color=""        #type:str
+        self.equiped=0       #type:int
     def __init__(self,pid,name,cls,ability,skills,pic,weapon_rank_bonus,items,support,cls_weapon_rank,color,attr):
         self.pid=pid
         self.name=name
@@ -27,6 +28,7 @@ class Person:
         self.supdata={}
         self.color=color
         self.attribute=attr
+        self.equiped=0
         if len(support)>0:
             for obj in support:
                 self.suprank[obj]=0
@@ -47,8 +49,41 @@ class Person:
         for weap in self.item:
             if is_weapon(weap.itemtype.weapontype):
                 if weap.itemtype.rank<=self.weapon_rank[weap.itemtype.weapontype]:
+                    if self.equiped==0:
+                        self.equip(weap)
                     return weap
         return None
+
+    def equip(self,_item):
+        if self.equiped==1:
+            eqp=self.get_equip()
+            self.dequip(eqp)
+        equip_item=_item
+        if equip_item not in self.item:
+            self.item.append(equip_item)
+        p=self.item.index(equip_item)
+        self.item[p]=self.item[0]
+        self.item[0]=equip_item
+        for abl in self.ability:
+            self.ability[abl]+=equip_item.itemtype.ability_bonus[abl]
+        self.equiped=1
+        return
+
+    def dequip(self,_item):
+        dequip_item=_item   #type:item.Item
+        for abl in self.ability:
+            self.ability[abl]-=dequip_item.itemtype.ability_bonus[abl]
+        if self.ability["HP"]>self.ability["MHP"]:
+            self.ability["HP"]=self.ability["MHP"]
+        self.equiped=0
+        return
+
+    def banish(self,_item):
+        if self.get_equip()==_item:
+            self.dequip(_item)
+        self.item.remove(_item)
+        del(_item)
+        return
 
 class PersonBank:
     def __init__(self):
