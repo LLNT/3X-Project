b17743f7aaeb02fa115b6fb5d8aa6a7f5f352073
unknown <ned1_32@sina.com>
2018-02-14 19:07:08 +0800

    map0 map and map tool csv2json

---
+++ tools/csv2json.py
@@ -0,0 +1,26 @@
+import json
+mapname="map_0"
+lines=open("%s.csv"%(mapname),"r").readlines()
+n=len(lines)
+m=len(lines[0].split(","))
+mat=[]
+d={}
+for i in range(m):
+    vec=[]
+    for j in range(n):
+        vec.append("")
+    mat.append(vec)
+    del(vec)
+for i in range(n):
+    line=lines[i].split(",")
+    for j in range(m):
+        block=line[j]
+        block=block.replace("\n","")
+        mat[j][n-i-1]=block
+d["Name"]=mapname
+d["M"]=m
+d["N"]=n
+d["Map"]=mat
+f=open("%s.json"%(mapname),"w")
+json.dump(d,f)
+f.close()
