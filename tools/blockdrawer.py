584534df39ea4c7131b7932d188393527413510f
Egria <ned1_32@sina.com>
2018-02-14 19:33:12 +0800

    Update blockdrawer.py

--- tools/blockdrawer.py
+++ tools/blockdrawer.py
@@ -6,5 +6,5 @@ c=(134,132,133)
 mat=np.zeros((80*m,80*n,3),dtype="uint8")
 for i in range(m):
     for j in range(n):
-        cv2.rectangle(mat,(80*i,80*j),(80,80),c)
+        cv2.rectangle(mat,(80*j,80*i),(80,80),c)
 cv2.imwrite("blocks.jpg",mat)
